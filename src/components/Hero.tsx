d82726ffc14db10ea936f0a0a643ca76845e6424
Reuben-Stephen-John <reuben.sj2003@gmail.com>
2023-08-09 23:46:50 +0530

    home page first part done

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+'use client'
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 import { BsArrowRight } from 'react-icons/bs';
 const Hero = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const buttonVariants = {
+    moving: { y: -10 },
+    stopped: { y: 0 },
+  };
   return (
     <div>
       <div className="w-full sm:h-50">
@@ -9,7 +17,7 @@ const Hero = () => {
           className="h-screen w-screen object-cover blur-1xl"
           alt="Background"
         />
-        
+
         {/* Top left logo outline */}
         <div className="absolute top-16 left-16 sm:top-8 left-8">
           <img
@@ -18,7 +26,7 @@ const Hero = () => {
             className="w-full h-20 md:10 sm:h-12"
           />
         </div>
-        
+
         {/* Top right logo outline */}
         <div className="absolute top-16 right-20 sm:top-8 right-6">
           <img
@@ -27,24 +35,40 @@ const Hero = () => {
             className="w-full h-30 md:10 sm:h-12"
           />
         </div>
-        
+
         {/* Centered text */}
         <div className="absolute inset-0 flex flex-col justify-center items-center text-center">
           <h1 className="text-9xl sm:text-7xl md:text-7xl font-monty bg-clip-text text-transparent bg-gradient-to-t from-stone-600 to-white">
             TECHNOVIT<span className="text-6xl sm:text-xl md:text-5xl">'23</span>
           </h1>
-          
+
           {/* Small Text */}
-          <div className="text-white text-base sm:text-sm text-6xl font-light italic capitalize text-center mt-6 sm:text-left">
-          light of future it's AI
-        </div>
+          <div className="text-white text-base sm:text-sm text-xl font-monty italic capitalize text-center mt-6 sm:text-left">
+            light of future it's AI
+          </div>
           {/* Button */}
-          <button className="w-50 h-10 sm:w-23 sm:h-10 rounded-full p-6 border border-white mt-6 flex items-center justify-center">
-            <div className="text-white flex gap-2 text-sm font-normal capitalize">
-              <p >Register Now</p>
-              <p><BsArrowRight size={20} /></p>
+          <motion.button
+            className="w-60 h-16 sm:w-23 h-10 rounded-full p-2 border border-white mt-20 flex items-center justify-center"
+            onHoverStart={() => setIsHovered(true)}
+            onHoverEnd={() => setIsHovered(false)}
+            variants={buttonVariants}
+            initial="moving"
+            animate={isHovered ? 'stopped' : 'moving'}
+            whileHover="stopped"
+          >
+            <div className="text-white flex gap-2 font-normal capitalize">
+              <p>Register Now</p>
+              <p>
+                <motion.span
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.2 }}
+                >
+                  <BsArrowRight size={20} />
+                </motion.span>
+              </p>
             </div>
-          </button>
+          </motion.button>
 
         </div>
 
