358033806bb006f49b9b9ac7a3908a586d861a93
Abhijith Ganesh <git.watch.abhijith@outlook.com>
2023-08-09 19:20:23 +0530

    Responsiveness patch🚀 (#3)
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,22 +2,24 @@ const Footer = (): JSX.Element => {
   return (
     <>
       <section className="flex flex-wrap justify-between px-32 text-white font-monty py-4">
-        <section className="flex flex-col gap-4">
-          <section className="font-semibold text-6xl">Contact Us!</section>
+        <section className="flex flex-col gap-4 md:py-8">
+          <section className="font-semibold text-6xl md:text-5xl sm:text-4xl">
+            Contact Us!
+          </section>
           <a
             href="mailto:technovit-help@vit.ac.in"
-            className="font-regular text-3xl pt-8 hover:underline"
+            className="font-regular text-3xl pt-8 hover:underline md:text-2xl sm:text-xl text-slate-100"
           >
             technovit-help@vit.ac.in
           </a>
           <a
             href="ph:+91-999222939"
-            className="font-regular text-3xl hover:underline"
+            className="font-regular text-3xl md:text-2xl sm:text-xl hover:underline text-slate-100"
           >
             +91-999222939
           </a>
         </section>
-        <section className="flex flex-col uppercase font-monty text-white gap-[40px] text-3xl">
+        <section className="flex flex-col uppercase font-monty text-white gap-[40px] text-3xl md:text-2xl sm:text-xl md:py-8 sm:py-8 sm:border-white sm:border-t-white">
           <section>Home</section>
           <section>About</section>
           <section>Events</section>
@@ -26,7 +28,7 @@ const Footer = (): JSX.Element => {
         </section>
         <section />
       </section>
-      <section className="w-screen bg-primary text-white font-monty uppercase h-10 text-xl flex items-center justify-center py-2">
+      <section className="w-screen bg-primary text-white font-monty uppercase h-10 text-xl flex items-center justify-center py-2 sm:text-sm md:text-lg">
         Made with ❤️ by Team TechnoVIT © {new Date().getFullYear()}
       </section>
     </>
