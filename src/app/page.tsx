d5a7d4493b857ddadf7a7a08e8626f8b2dac18f5
Reuben-Stephen-John <reuben.sj2003@gmail.com>
2023-08-09 19:54:06 +0530

    home page base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,46 @@
+import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import HeroSection from "../components/hero";
+
 export default function Home() {
   return (
     <div>
-      <div className=" h-screen relative">
-        <div className="z-20 absolute w-full">
-          <Navbar />
-        </div>
+      <Navbar />
 
-        <div className="w-full">
-          {/* <h1 className="text-white">Hiiii</h1> */}
-          <video
-            src="/assets/purpleparticle2.mp4"
-            autoPlay
-            loop
-            muted
-            className="h-screen w-screen z-0 fixed object-cover"
-          ></video>
-          <div className="relative translate-y-20 text-center">
-            <svg height={"100%"} width={"100%"} className="fill-white">
-              <text x={"50%"} y={"100%"} textAnchor="middle">
-                technoVIT&apos;23
-              </text>
-            </svg>
+      <HeroSection/>
+
+      {/* Features Section */}
+      <section className="py-16 bg-gray-100">
+        <div className="container mx-auto text-center">
+          <h2 className="text-2xl font-bold mb-4">Our Features</h2>
+          <div className="grid grid-cols-3 gap-8">
+            {/* Feature 1 */}
+            <div className="bg-white p-6 rounded-md shadow-md">
+              <h3 className="text-lg font-semibold mb-2">Feature 1</h3>
+              <p className="text-gray-600">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
+            {/* Feature 2 */}
+            <div className="bg-white p-6 rounded-md shadow-md">
+              <h3 className="text-lg font-semibold mb-2">Feature 2</h3>
+              <p className="text-gray-600">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
+            {/* Feature 3 */}
+            <div className="bg-white p-6 rounded-md shadow-md">
+              <h3 className="text-lg font-semibold mb-2">Feature 3</h3>
+              <p className="text-gray-600">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="relative">
-        <Footer />
-      </div>
+      </section>
+      
+      <Footer />
     </div>
   );
 }
