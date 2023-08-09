1a8819a1412eb39f8e44a58b6e30ee790ad4630a
RITESHP36 <codingritesh36@gmail.com>
2023-08-09 22:17:34 +0530

    fixed text size
    Made font size match with the contact us section as per request

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,15 +3,18 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-        <div className="md:grid md:grid-cols-3  md:gap-8 lg:px-36 md:px-30 sm:14  px-12">
-            {/* left */}
-            <div className="">
-                <h2 className="text-gradient-about md:text-end text-center  md:text-[60px] sm:text-[50px] text-[40px] ">ABOUT</h2>
-            </div>
-            {/* right */}
-            <div className="text-white font-extralight text-justify md:text-[18px] text-[15px] col-span-2 py-3">
+        <div className="md:grid md:grid-cols-3  md:gap-8 lg:px-36 md:px-30   px-12 ">
+         {/* left */}
+         <div className="">
+	         <h2 className="text-gradient-about md:text-end text-center  md:text-6xl sm:text-5xl text-4xl ">ABOUT</h2>
+	    </div>
+	            {/* right */}
+	    {/* <p className="leading- text-white font-extralight text-justify md:text-lg text-[15px] col-span-2 py-3"> */}
+        <div className='col-span-2'>
+        <p className=" text-white font-extralight text-justify md:text-lg text-base !leading-tight">
             This is the 7th Edition of technoVIT, an international level technical festival of VIT Chennai.It gives us immense pride and pleasure in presenting technoVIT' 23. This technical extravaganza will be filled with numerous activities such as workshops and technical events organised by the various schools of the University with the primary objective being, " nurture professional skills and impart fine qualities to the students". It also has gaming events to thrill and create an environment with fun and frolic. Additionally, technical exhibitions from various organisations are being hosted. We, at VIT Chennai promise the audience an atmosphere of immense joy leaving them the thirst for more
-            </div>
+            </p>
+        </div>
         </div>
     </div>
   )
