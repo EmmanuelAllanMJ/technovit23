63176d97e0bcb88540fac48fbf7a1324a709f63b
EmmanuelAllanMJ <emmanuelallanmj@gmail.com>
2023-08-10 11:11:49 +0530

    Fixed gallery images

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -7,17 +7,17 @@ import styles from './page.module.scss';
 
 const images = [
   "1.jpg",
-  "2.jpg",
-  "3.jpg",
-  "4.jpg",
-  "5.jpg",
-  "6.jpg",
-  "7.jpg",
-  "8.jpg",
-  "9.jpg",
-  "10.jpg",
-  "11.jpg",
-  "12.jpg",
+  "2.JPG",
+  "3.JPG",
+  "4.JPG",
+  "5.JPG",
+  "6.JPG",
+  "7.JPG",
+  "8.JPG",
+  "9.JPG",
+  "10.JPG",
+  "11.JPG",
+  "12.JPG",
 ];
 
 const Gallery: React.FC = () => {
@@ -81,6 +81,7 @@ const Column: React.FC<ColumnProps> = ({ images, y }) => {
           <Image src={`/images/${src}`} alt='image' fill objectFit="cover" />
         </div>
       ))}
+
     </motion.div>
   );
 };
