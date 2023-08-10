9798d0e8b30dca347bdcedc08a8f3aec5737e148
Reuben-Stephen-John <reuben.sj2003@gmail.com>
2023-08-10 09:58:18 +0530

    new image gallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Hero from "@/components/Hero";
 import Footer from "@/components/Footer";
 import Space from "@/components/Space";
+import Gallery from "@/components/Gallery";
+import Cards from "@/components/Cards";
 
 export default function Home() {
   return (
     <div className="bg-[#110F13]">
       <Hero />
-      <Space />
+      <Gallery/>
       <Footer />
     </div>
   );
