d2938bc9fede0bdd8046ff49ef23347b86f17ff5
Reuben-Stephen-John <reuben.sj2003@gmail.com>
2023-08-10 17:22:49 +0530

    vercel will deploy now...

--- src/components/ticker.tsx
+++ src/components/ticker.tsx
@@ -1,26 +1,26 @@
 "use client";
-import Ticker from "react-ticker";
+// import Ticker from "react-ticker";
 
-let TickerComponent = (): JSX.Element => {
-  return (
-    <section className="bg-slate-200">
-      <Ticker speed={2}>
-        {({ index }) => (
-          <section className="flex">
-            <section className="text-white font-semibold font-monty text-2xl px-2">
-              | ChennAI's next biggest StartUp event
-            </section>
-            <section className="text-white font-semibold font-monty text-2xl px-2">
-              | VIT Chennai
-            </section>
-            <section className="text-white font-semibold font-monty text-2xl px-2">
-              | Light of the Future, its AI
-            </section>
-          </section>
-        )}
-      </Ticker>
-    </section>
-  );
-};
+// let TickerComponent = (): JSX.Element => {
+//   return (
+//     <section className="bg-slate-200">
+//       <Ticker speed={2}>
+//         {({ index }) => (
+//           <section className="flex">
+//             <section className="text-white font-semibold font-monty text-2xl px-2">
+//               | ChennAI's next biggest StartUp event
+//             </section>
+//             <section className="text-white font-semibold font-monty text-2xl px-2">
+//               | VIT Chennai
+//             </section>
+//             <section className="text-white font-semibold font-monty text-2xl px-2">
+//               | Light of the Future, its AI
+//             </section>
+//           </section>
+//         )}
+//       </Ticker>
+//     </section>
+//   );
+// };
 
-export default TickerComponent;
+// export default TickerComponent;
