fda61be573f5c7f3edf58aca00f55e30874b29d1
Abhijith Ganesh <git.watch.abhijith@outlook.com>
2023-08-10 19:23:47 +0530

    Optimizaton Patch 1✨
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,22 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
     <div>
-        <div className="flex justify-between md:flex-col md:gap-6 sm:flex-col sm:gap-4 px-32 md:px-28 sm:px-16 py-4">
-          {/* left */}
-          <div className="lg:w-4/12">
-            <h2 className="font-monty bg-clip-text text-transparent bg-gradient-to-t from-stone-600 to-white text-center  text-8xl md:text-6xl sm:text-5xl  ">ABOUT</h2>
-          </div>
-          {/* right */}
-          <div className='lg:w-8/12'>
-            <p className=" text-neutral-100 font-monty font-extralight text-justify text-3xl md:text-2xl sm:text-lg !leading-tight">
-                This is the 7th Edition of technoVIT, an international level technical festival of VIT Chennai.It gives us immense pride and pleasure in presenting technoVIT' 23. This technical extravaganza will be filled with numerous activities such as workshops and technical events organised by the various schools of the University with the primary objective being, " nurture professional skills and impart fine qualities to the students". It also has gaming events to thrill and create an environment with fun and frolic. Additionally, technical exhibitions from various organisations are being hosted. We, at VIT Chennai promise the audience an atmosphere of immense joy leaving them the thirst for more
-            </p>
-          </div>
+      <div className="flex justify-between md:flex-col md:gap-6 sm:flex-col sm:gap-4 px-32 md:px-28 sm:px-16 py-4">
+        <div className="">
+          <h2 className="font-monty bg-clip-text text-transparent bg-gradient-to-tl from-stone-600 to-white text-center text-6xl md:text-6xl sm:text-5xl uppercase">
+            About
+          </h2>
         </div>
+        <div className="lg:w-8/12">
+          <p className=" text-neutral-100 font-monty font-extralight text-justify text-3xl md:text-2xl sm:text-lg !leading-tight">
+            This is the 7th Edition of technoVIT, an international level
+            technical festival of VIT Chennai.It gives us immense pride and
+            pleasure in presenting technoVIT' 23. This technical extravaganza
+            will be filled with numerous activities such as workshops and
+            technical events organised by the various schools of the University
+            with the primary objective being, " nurture professional skills and
+            impart fine qualities to the students". It also has gaming events to
+            thrill and create an environment with fun and frolic. Additionally,
+            technical exhibitions from various organisations are being hosted.
+            We, at VIT Chennai promise the audience an atmosphere of immense joy
+            leaving them the thirst for more
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
