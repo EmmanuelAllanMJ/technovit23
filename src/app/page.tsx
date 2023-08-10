85b25a4ba93a6b124924b344ccb8cdba2ea4ed25
Abhijith Ganesh <git.watch.abhijith@outlook.com>
2023-08-10 11:17:15 +0530

    Addition of new schools
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-<!-- import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar";
 import Schools from "@/components/schools";
 import Footer from "@/components/Footer";
 import TickerComponent from "@/components/ticker";
@@ -37,30 +37,6 @@ export default function Home() {
       <div className="relative">
         <Footer />
       </div>
- -->
-
-import Hero from "@/components/Hero";
-import Footer from "@/components/Footer";
-import Space from "@/components/Space";
-
-
-import About from "@/components/About";
-import dynamic from "next/dynamic";
-
-const Countdown = dynamic(() => import('../components/Countdown'), {
-  ssr: false
-})
-
-export default function Home() {
-  return (
-    <div className="bg-[#110F13]">
-      <Hero />
-      <Space />
-
-      <Countdown/>
-      <About />
-
-      <Footer />
     </div>
   );
 }
