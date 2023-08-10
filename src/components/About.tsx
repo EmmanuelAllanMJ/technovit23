e3bc29477ecc197b8f7543a1ec55542e50df4332
RITESHP36 <codingritesh36@gmail.com>
2023-08-10 09:13:13 +0530

    About section added

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,18 +3,17 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-        <div className="md:grid md:grid-cols-3  md:gap-8 lg:px-36 md:px-30   px-12 ">
-         {/* left */}
-         <div className="">
-	         <h2 className="text-gradient-about md:text-end text-center  md:text-6xl sm:text-5xl text-4xl ">ABOUT</h2>
-	    </div>
-	            {/* right */}
-	    {/* <p className="leading- text-white font-extralight text-justify md:text-lg text-[15px] col-span-2 py-3"> */}
-        <div className='col-span-2'>
-        <p className=" text-white font-extralight text-justify md:text-lg text-base !leading-tight">
-            This is the 7th Edition of technoVIT, an international level technical festival of VIT Chennai.It gives us immense pride and pleasure in presenting technoVIT' 23. This technical extravaganza will be filled with numerous activities such as workshops and technical events organised by the various schools of the University with the primary objective being, " nurture professional skills and impart fine qualities to the students". It also has gaming events to thrill and create an environment with fun and frolic. Additionally, technical exhibitions from various organisations are being hosted. We, at VIT Chennai promise the audience an atmosphere of immense joy leaving them the thirst for more
+        <div className="flex justify-between md:flex-col md:gap-6 sm:flex-col sm:gap-4 px-32 md:px-28 sm:px-16 py-4">
+          {/* left */}
+          <div className="lg:w-4/12">
+            <h2 className="font-monty bg-clip-text text-transparent bg-gradient-to-t from-stone-600 to-white text-center  text-8xl md:text-6xl sm:text-5xl  ">ABOUT</h2>
+          </div>
+          {/* right */}
+          <div className='lg:w-8/12'>
+            <p className=" text-neutral-100 font-monty font-extralight text-justify text-3xl md:text-2xl sm:text-lg !leading-tight">
+                This is the 7th Edition of technoVIT, an international level technical festival of VIT Chennai.It gives us immense pride and pleasure in presenting technoVIT' 23. This technical extravaganza will be filled with numerous activities such as workshops and technical events organised by the various schools of the University with the primary objective being, " nurture professional skills and impart fine qualities to the students". It also has gaming events to thrill and create an environment with fun and frolic. Additionally, technical exhibitions from various organisations are being hosted. We, at VIT Chennai promise the audience an atmosphere of immense joy leaving them the thirst for more
             </p>
-        </div>
+          </div>
         </div>
     </div>
   )
