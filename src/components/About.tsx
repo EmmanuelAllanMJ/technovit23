82009091fb89c6f3fafd87a660569ebbcc0f3dd1
404salad <404salad@gmail.com>
2023-08-12 12:16:58 +0530

    changed about to the new design

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,21 +2,21 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <div className="flex justify-between md:flex-col md:gap-6 sm:flex-col sm:gap-4 px-32 md:px-28 sm:px-16 md:pt-60 sm:pt-10 h-full">
-        <div className="">
+    <div className="">
+      <div className="flex px-32 md:px-28 sm:px-16 md:pt-60 sm:pt-10 h-screen flex-col justify-center">
+        <div className="mt-8">
           <h2 className="font-monty bg-clip-text text-transparent bg-gradient-to-tl from-stone-600 to-white text-center text-6xl md:text-6xl sm:text-5xl uppercase">
-            About
+            About technoVIT
           </h2>
         </div>
-        <div className="lg:w-8/12">
-          <p className=" text-neutral-100 font-monty font-extralight text-justify md:text-left sm:text-left text-3xl md:text-2xl sm:text-lg !leading-normal">
-            This is the 7th Edition of technoVIT, an international level
-            technical festival of VIT Chennai.It gives us immense pride and
+        <div className="grid place-items-center">
+          <p className=" text-neutral-100 font-monty font-extralight text-justify lg:w-10/12 text-3xl md:text-2xl sm:text-lg !leading-normal pt-4">
+            This is the 7<sup>th</sup> Edition of technoVIT, an international level
+            technical festival of VIT Chennai. It gives us immense pride and
             pleasure in presenting technoVIT' 23. This technical extravaganza
             will be filled with numerous activities such as workshops and
             technical events organised by the various schools of the University
-            with the primary objective being, " nurture professional skills and
+            with the primary objective being, "nurture professional skills and
             impart fine qualities to the students". It also has gaming events to
             thrill and create an environment with fun and frolic. Additionally,
             technical exhibitions from various organisations are being hosted.
