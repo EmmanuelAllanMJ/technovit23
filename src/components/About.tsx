cc9766e190917990f660f731c726727beb6fd72a
404salad <110289934+404salad@users.noreply.github.com>
2023-08-13 02:35:38 +0530

    Finished about section (#31)
    * changed about to the new design
    
    * Update yarn.lock
    
    * finished about
    
    ---------

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,21 +2,24 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <div className="flex justify-between md:flex-col md:gap-6 sm:flex-col sm:gap-4 px-32 md:px-28 sm:px-16 md:pt-60 sm:pt-10 h-full">
-        <div className="">
-          <h2 className="font-monty bg-clip-text text-transparent bg-gradient-to-tl from-stone-600 to-white text-center text-6xl md:text-6xl sm:text-5xl uppercase">
-            About
+    <div className="about-grid">
+     <br/>
+      <div className="flex px-32 md:px-28 sm:px-16 md:pt-60 sm:pt-10 h-screen md:h-fit flex-col justify-center">
+        <div className="mt-8">
+          <img src="/assets/stars.svg" className="w-28 sm:w-16 mt-4"></img>
+          <h2 className="z-100 font-monty bg-clip-text text-transparent bg-gradient-to-tl from-stone-600 to-white text-center text-6xl md:text-6xl sm:text-5xl uppercase">
+            About technoVIT
           </h2>
         </div>
-        <div className="lg:w-8/12">
-          <p className=" text-neutral-100 font-monty font-extralight text-justify md:text-left sm:text-left text-3xl md:text-2xl sm:text-lg !leading-normal">
-            This is the 7th Edition of technoVIT, an international level
-            technical festival of VIT Chennai.It gives us immense pride and
+        <div className="grid place-items-center">
+          <div className=" absolute right-1/3 shadow-purple-900 opacity-30 md:opacity:10 sm:opacity:5 w-300 h-200 shadow-[0_0_20em_20em] "></div>
+          <p className="z-10 text-neutral-100 font-monty font-extralight text-justify mt-6 lg:w-10/12 text-3xl md:text-2xl sm:text-lg !leading-normal pt-4">
+            This is the 7<sup>th</sup> Edition of technoVIT, an international level
+            technical festival of VIT Chennai. It gives us immense pride and
             pleasure in presenting technoVIT' 23. This technical extravaganza
             will be filled with numerous activities such as workshops and
             technical events organised by the various schools of the University
-            with the primary objective being, " nurture professional skills and
+            with the primary objective being, "nurture professional skills and
             impart fine qualities to the students". It also has gaming events to
             thrill and create an environment with fun and frolic. Additionally,
             technical exhibitions from various organisations are being hosted.
