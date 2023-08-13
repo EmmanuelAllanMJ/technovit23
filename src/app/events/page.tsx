e22d9bf01c8f8fc2f3d2eb4a6b281590d6fcef33
Kushagra Garg <kushagra.garg77@gmail.com>
2023-08-13 23:27:04 +0530

    backdrop fixed

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -3,13 +3,15 @@ import React from "react";
 
 const EventsPage = () => {
   return (
-    <div className="bg-black">
+    <div className="bg-black relative about-grid pt-6">
       <img
         src="/assets/events-bg.png"
-        className="h-screen w-screen object-cover blur-1xl"
+        className="h-screen w-screen object-cover blur-1xl absolute top-0 left-0 z-0"
         alt="Background"
       />
-      <Events />
+      <div className="relative z-10">
+        <Events />
+      </div>
     </div>
   );
 };
