228a5891b1f3143684e47a7eaaff8b9632f63718
Reuben-Stephen-John <reuben.sj2003@gmail.com>
2023-08-14 12:09:11 +0530

    register link added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -106,12 +106,12 @@ const Hero = () => {
         </motion.span>
       </div>
           <div className="text-white text-3xl sm:text-xl font-monty italic mt-4">Light Of Future - AI</div>
-          <button className="mt-8 ml-2 rounded-[90px] border-2 border-purple-600 w-52
+          <a href='https://vitchennaievents.com/conf1/' target="_blank" className="mt-8 ml-2 rounded-[90px] border-2 border-purple-600 w-52
            h-16 sm:w-44 sm:h-14 md:w-48 md:h-16 lg:w-52 lg:h-16 flex justify-center 
            items-center text-[#C8B8EC] text-base font-medium cursor-pointer 
            bg-opacity-80 hover:bg-purple-500/10 hover:text-purple-200 transition-all duration-300">
             <div className="text-[#C8B8EC] text-base font-medium">Register Now</div>
-          </button>
+          </a>
         </div>
       </div>
     </div>
