01e1e138b9496665b41d11d3631a9939ca8f0fb5
404salad <110289934+404salad@users.noreply.github.com>
2023-08-15 00:34:01 +0530

    fixed coordinator responsiveness and added student section (#44)
    * fixed carousel
    
    * fixed coordinators

--- src/components/Coordinator.tsx
+++ src/components/Coordinator.tsx
@@ -1,20 +1,24 @@
 "use client"
 import { FC } from 'react'
 import Carousel from './Carousel'
+import CarouselStudents from './CarouselStudents'
 
 const Coordinator = () => {
     return (
-        <div className='overflow-hidden'>
+        <div className='font-monty overflow-hidden'>
             <img
                 src="/assets/coordinates.png"
-                className="object-cover blur-1xl"
+                className="mx-auto blur-1xl"
                 alt="Background"
             />
-            <h2 className='-mt-48 md:-mt-96 text-3xl md:text-5xl text-white uppercase text-center md:uppercase'>Co-ordinators</h2>
+            <h2 className=' mt-100 mb-10 text-3xl md:text-5xl lg:text-6xl text-white uppercase text-center'>Co-ordinators</h2>
             
-            <div className='mt-16'>
+            <div className='mt-1'>
                 <Carousel />
+                <br />
+                <CarouselStudents />
             </div>
+            <br />
         </div>
     )
 }
