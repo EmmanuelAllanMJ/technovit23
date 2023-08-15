0c2057f7b4f3bc67b94d7e688a75fdef9ca4e5e8
EmmanuelAllanMJ <emmanuelallanmj@gmail.com>
2023-08-15 14:20:22 +0530

    Modified carousel

---
+++ public/data/data.ts
@@ -0,0 +1,45 @@
+export const studentImageUrl: Array<{ link: string, name: string }> = [
+    {
+        link: "/images/student/MONISH.JPG",
+        name: "Mohish S",
+    },
+    {
+        link: '/images/student/Nivedhitha.jpg',
+        name: "Niveditha Gna",
+    }, {
+        link: '/images/student/Jayasree.jpg',
+        name: " Jayasree M",
+    }, {
+        link: "/images/student/Harshadaa.jpg",
+        name: "Harshadaavenkat"
+    },
+    {
+        link: "/images/student/shyam.jpg",
+        name: "Shyam Sundhar",
+    },
+    {
+        link: "/images/student/Aadil.jpg",
+        name: "Mohammed Aadil",
+    },
+    {
+        link: "/images/student/Pravarthika.jpeg",
+        name: "Pravarthika V",
+    }
+];
+
+export const facultyImageUrl: Array<{ link: string, name: string }> = [
+
+    {
+        name: "Dr. Vijayakumar P",
+        link: "/images/faculty/Vijayakumar.jpg"
+    },
+    {
+        name: "Dr. Karmel A",
+        link: "/images/faculty/Karmel.png"
+    },
+    {
+        name: "Dr. S. Bharathiraja",
+        link: "/images/faculty/Bharathiraja.jpg"
+    },
+
+]
--- src/components/Coordinator.tsx
+++ src/components/Coordinator.tsx
@@ -1,9 +1,14 @@
 "use client"
-import { FC } from 'react'
-import Carousel from './Carousel'
-import CarouselStudents from './CarouselStudents'
+import { FC, useRef } from 'react'
+// import CarouselStudents from './CarouselStudents'
+import Carousel from "framer-motion-carousel"
+import { facultyImageUrl, studentImageUrl } from '../../public/data/data';
+import Image from 'next/image';
+
 
 const Coordinator = () => {
+    const carouselRef = useRef();
+
     return (
         <div className='font-monty overflow-hidden'>
             <img
@@ -12,13 +17,61 @@ const Coordinator = () => {
                 alt="Background"
             />
             <h2 className=' mt-100 mb-10 text-3xl md:text-5xl lg:text-6xl text-white uppercase text-center'>Co-ordinators</h2>
-            
+            {/*             
             <div className='mt-1'>
                 <Carousel />
                 <br />
                 <CarouselStudents />
             </div>
-            <br />
+            <br /> */}
+            <>
+                <div className="flex w-full">
+                    <h1 className="text-white mx-auto text-4xl my-16">Faculty</h1>
+                </div>
+
+                <div style={{ width: 800, height: 500, margin: "0 auto" }} >
+                    <Carousel autoPlay={true} interval={20000} loop={true} >
+                        {facultyImageUrl.map((item, i) => (
+                            <div className='flex items-center justify-center flex-col'>
+                                <Image
+                                    draggable="false"
+                                    src={item.link}
+                                    key={i}
+                                    width={400}
+                                    height={400}
+                                    alt={item.name}
+                                />
+                                <h1 className="text-white text-center text-3xl">{item.name}</h1>
+
+                            </div >
+                        ))}
+                    </Carousel>
+                </div>
+
+                <div className="flex w-full">
+                    <h1 className="text-white mx-auto text-4xl my-16">Student</h1>
+                </div>
+
+                <div style={{ width: 800, height: 500, margin: "0 auto" }} >
+                    <Carousel autoPlay={true} interval={20000} loop={true}>
+                        {studentImageUrl.map((item, i) => (
+                            <div className='flex items-center justify-center flex-col'>
+                                <Image
+                                    draggable="false"
+                                    src={item.link}
+                                    key={i}
+                                    width={400}
+                                    height={400}
+                                    alt={item.name}
+                                />
+                                <h1 className="text-white text-center text-3xl">{item.name}</h1>
+
+                            </div >
+                        ))}
+                    </Carousel>
+                </div>
+
+            </>
         </div>
     )
 }
