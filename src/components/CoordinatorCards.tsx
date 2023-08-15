ca205401fccda1554b22ac997bee6d802dcadb66
Abhijith Ganesh <git.watch.abhijith@outlook.com>
2023-08-15 20:02:10 +0530

    Addition of new changes
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>

--- src/components/CoordinatorCards.tsx
+++ src/components/CoordinatorCards.tsx
@@ -1,58 +1,12 @@
 import Image from "next/image";
 
-type ICardProps = {
+export type ICardProps = {
   link: string;
   name: string;
   className?: string;
 };
 
-const imageUrl: ICardProps[] = [
-  {
-    link: "/images/student/MONISH.JPG",
-    name: "Monish S",
-  },
-  {
-    link: "/images/student/Nivedhitha.jpg",
-    name: "Niveditha Gna",
-  },
-  {
-    link: "/images/student/Jayasree.jpg",
-    name: " Jayasree M",
-  },
-  {
-    link: "/images/student/Harshadaa.jpg",
-    name: "Harshadaavenkat",
-  },
-  {
-    link: "/images/student/shyam.jpg",
-    name: "Shyam Sundhar",
-  },
-  {
-    link: "/images/student/Aadil.jpg",
-    name: "Mohammed Aadil",
-  },
-  {
-    link: "/images/student/Pravarthika.jpeg",
-    name: "Pravarthika V",
-  },
-];
-
-const facultyImageUrl: ICardProps[] = [
-  {
-    name: "Dr. Vijayakumar P",
-    link: "/images/faculty/Vijayakumar.jpg",
-  },
-  {
-    name: "Dr. Karmel A",
-    link: "/images/faculty/Karmel.png",
-  },
-  {
-    name: "Dr. S. Bharathiraja",
-    link: "/images/faculty/Bharathiraja.jpg",
-  },
-];
-
-function CoordinatorCardImage(props: ICardProps): JSX.Element {
+const CoordinatorCardImage = (props: ICardProps): JSX.Element => {
   return (
     <section
       className={`${props.className} flex flex-col gap-4 py-4 justify-center items-center object-contain`}
@@ -69,5 +23,5 @@ function CoordinatorCardImage(props: ICardProps): JSX.Element {
       </h1>
     </section>
   );
-}
+};
 export default CoordinatorCardImage;
