35c17aa187deae605ec24e7396834360d3ab9796
Reuben-Stephen-John <reuben.sj2003@gmail.com>
2023-08-16 15:43:47 +0530

    about text tweaks

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <>
-      <div className="flex px-32 md:px-28 sm:px-16  flex-col justify-center mt-12 mb-16 ">
+      <div className="flex px-32 md:px-28 sm:px-16  flex-col justify-center mt-12 mb-16">
         <div className="">
           <div className="absolute animate-pulse">
             <svg>circle</svg>
@@ -19,18 +19,7 @@ const About = () => {
         <div className="grid place-items-center">
           <div className=" absolute right-1/3 shadow-purple-900 opacity-30 md:opacity:10 sm:opacity:5 w-300 h-200 shadow-[0_0_20em_20em] "></div>
           <p className="z-10 text-neutral-100 font-monty font-extralight text-justify mt-6 lg:w-10/12 text-4xl md:text-3xl sm:text-2xl pt-4">
-            This is the 8<sup>th</sup> Edition of TechnoVIT, an international
-            level technical festival of VIT Chennai. It gives us immense pride
-            and pleasure in presenting TechnoVIT' 23. This technical
-            extravaganza will be filled with numerous activities such as
-            workshops and technical events organised by the various schools of
-            the University with the primary objective being, "nurture
-            professional skills and impart fine qualities to the students". It
-            also has gaming events to thrill and create an environment with fun
-            and frolic. Additionally, technical exhibitions from various
-            organisations are being hosted. We, at VIT Chennai promise the
-            audience an atmosphere of immense joy leaving them the thirst for
-            more.
+            This is the 8<sup>th</sup> Edition of TechnoVIT, an international level technical festival of VIT Chennai. It gives us immense pride and pleasure in presenting TechnoVIT' 23. This technical extravaganza will be filled with numerous activities such as workshops and technical events organised by the various schools of the University with the primary objective being, "nurture professional skills and impart fine qualities to the students". It also has gaming events to thrill and create an environment with fun and frolic. Additionally, technical exhibitions from various organisations are being hosted. We, at VIT Chennai promise the audience an atmosphere of immense joy leaving them the thirst for more.
           </p>
           <br />
         </div>
