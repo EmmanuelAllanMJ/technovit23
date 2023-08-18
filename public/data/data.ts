3f16f16f9518256cdd9244d7490d99d5a9ce3b82
EmmanuelAllanMJ <emmanuelallanmj@gmail.com>
2023-08-18 16:10:29 +0530

    Added modal for schools

--- public/data/data.ts
+++ public/data/data.ts
@@ -7,6 +7,20 @@ export type CardProps = {
   description: string;
 };
 
+export type ImageProps = {
+  image: string;
+  className?: string;
+  name: string;
+  description: string;
+  faculty: {
+    name: string;
+    designation: string;
+    description: string;
+    link: string;
+    email: string;
+  }[];
+}
+
 export const studentImageUrl: ICardProps[] = [
   {
     link: "/images/student/MONISH.JPG",
@@ -101,3 +115,238 @@ export const aboutFacultyImageUrl: CardProps[] = [
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit."
   }
 ]
+
+// VFIT GLIT
+// 1. Dr. S. G. Deepika (51965)
+// 2. Dr. M. Bhuvaneshwari (51973)
+// SMEC Diseno
+// 1. Dr. Giridharan (50730)
+// 2. Dr. Muralimohan (51727)
+
+export const schools:ImageProps[] = [
+  {
+    name : "qubit",
+    description : "Qubit is the school of Computer Science and Engineering at VIT Chennai. It is one of the most active schools in the campus. The school has a very active student body and has been organizing various events and workshops for the stuents",
+    image : "/logos/qubit.webp",
+    faculty : [
+      {
+        name : "Dr. Anusha K",
+        designation : "Associate Professor",
+        description : "Dr. Anusha K is an Associate Professor in the School of Computer Science and Engineering at VIT Chennai. She has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Anusha.jpg",
+        email : "anusha.k@vit.ac.in"
+      },
+      {
+        name : "Dr. M. Prasad",
+        designation : "Associate Professor",
+        description : "Dr. M. Prasad is an Associate Professor in the School of Computer Science and Engineering at VIT Chennai. He has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Prasad.jpg",
+        email : ""
+      },
+      {
+        name : "Dr. Sandosh S",
+        designation : "Associate Professor",
+        description : "Dr. Sandosh S is an Associate Professor in the School of Computer Science and Engineering at VIT Chennai. He has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Sandosh.jpg",
+        email : ""
+      },
+      {
+        name : "Dr. Asha Jerlin M",
+        designation : "Associate Professor",
+        description : "Dr. Asha Jerlin M is an Associate Professor in the School of Computer Science and Engineering at VIT Chennai. She has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Asha.jpg",
+        email : ""
+      }
+    ]
+  },
+  {
+    name : "bandVIT",
+    description : "bandVIT is the school of Electrical and Electronics Engineering at VIT Chennai. It is one of the most active schools in the campus. The school has a very active student body and has been organizing various events and workshops for the stuents",
+    image : "/logos/bandvit.webp",
+    faculty : [
+      {
+        name : "Sofana Reka S",
+        designation : "Associate Professor",
+        description : "Sofana Reka S is an Associate Professor in the School of Electrical and Electronics Engineering at VIT Chennai. She has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Sofana.jpg",
+        email : ""
+      },
+      {
+        name : "Ravi Prakash Dwivedi",
+        designation : "Associate Professor",
+        description : "Ravi Prakash Dwivedi is an Associate Professor in the School of Electrical and Electronics Engineering at VIT Chennai. He has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Ravi.jpg",
+        email : ""
+      }
+    ]
+  },
+  {
+    name : "Glitz",
+    description : "Glitz is the school of Mechanical Engineering at VIT Chennai. It is one of the most active schools in the campus. The school has a very active student body and has been organizing various events and workshops for the stuents",
+    image : "/logos/glitz.webp",
+    faculty : [
+      {
+        name : "Dr. S. G. Deepika",
+        designation : "Associate Professor",
+        description : "Dr. S. G. Deepika is an Associate Professor in the School of Mechanical Engineering at VIT Chennai. She has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Deepika.jpg",
+        email : ""
+      },
+      {
+        name : "Dr. M. Bhuvaneshwari",
+        designation : "Associate Professor",
+        description : "Dr. M. Bhuvaneshwari is an Associate Professor in the School of Mechanical Engineering at VIT Chennai. She has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Bhuvaneshwari.jpg",
+        email : ""
+      }
+    ]
+  },
+  {
+    name : "Diseno",
+    description : "Diseno is the school of Electronics and Communication Engineering at VIT Chennai. It is one of the most active schools in the campus. The school has a very active student body and has been organizing various events and workshops for the stuents",
+    image : "/logos/diseno.webp",
+    faculty : [
+      {
+        name : "Dr. Giridharan",
+        designation : "Associate Professor",
+        description : "Dr. Giridharan is an Associate Professor in the School of Electronics and Communication Engineering at VIT Chennai. He has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Giridharan.jpg",
+        email : ""
+      },
+      {
+        name : "Dr. Muralimohan",
+        designation : "Associate Professor",
+        description : "Dr. Muralimohan is an Associate Professor in the School of Electronics and Communication Engineering at VIT Chennai. He has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Muralimohan.jpg",
+        email : ""
+      }
+    ]
+  },  
+  {
+    name : "ConnectiVITeee",
+    description : "ConnectiVITeee is the school of Electronics and Communication Engineering at VIT Chennai. It is one of the most active schools in the campus. The school has a very active student body and has been organizing various events and workshops for the stuents",
+    image : "/logos/connectivitieee.webp",
+    faculty : [
+      {
+        name : "Dr. Pritam Bhowmik",
+        designation : "Associate Professor",
+        description : "Dr. Pritam Bhowmik is an Associate Professor in the School of Electronics and Communication Engineering at VIT Chennai. He has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Pritam.jpg",
+        email : ""
+      },
+      {
+        name : "Dr. Jyotismita Mishra",
+        designation : "Associate Professor",
+        description : "Dr. Jyotismita Mishra is an Associate Professor in the School of Electronics and Communication Engineering at VIT Chennai. She has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Jyotismita.jpg",
+        email : ""
+      }
+    ]
+  },
+  {
+    name : "VSPLASH",
+    description : "VSPLASH is the school of Information Technology at VIT Chennai. It is one of the most active schools in the campus. The school has a very active student body and has been organizing various events and workshops for the stuents",
+    image : "/logos/vsplash.webp",
+    faculty : [
+      {
+        name : "Dr. Parthiban V",
+        designation : "Associate Professor",
+        description : "Dr. Parthiban V is an Associate Professor in the School of Information Technology at VIT Chennai. He has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Parthiban.jpg",
+        email : ""
+      },
+      {
+        name : "Dr. Dhanasekar S",
+        designation : "Associate Professor",
+        description : "Dr. Dhanasekar S is an Associate Professor in the School of Information Technology at VIT Chennai. He has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Dhanasekar.jpg",
+        email : ""
+      }
+    ]
+  },
+  {
+    name : "VITness",
+    description : "VITness is the school of Mechanical Engineering at VIT Chennai. It is one of the most active schools in the campus. The school has a very active student body and has been organizing various events and workshops for the stuents",
+    image : "/logos/vitness.webp",
+    faculty : [
+      {
+        name : "Dr. Jishnu J",
+        designation : "Associate Professor",
+        description : "Dr. Jishnu J is an Associate Professor in the School of Mechanical Engineering at VIT Chennai. He has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Jishnu.jpg",
+        email : ""
+      },
+      {
+        name : "Dr. Nikhil G",
+        designation : "Associate Professor",
+        description : "Dr. Nikhil G is an Associate Professor in the School of Mechanical Engineering at VIT Chennai. He has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Nikhil.jpg",
+        email : ""
+      }
+    ]
+  },
+  {
+    name : "SRISHTI",
+    description : "SRISHTI is the school of Science and Humanities at VIT Chennai. It is one of the most active schools in the campus. The school has a very active student body and has been organizing various events and workshops for the stuents",
+    image : "/logos/srishti.webp",
+    faculty : [
+      {
+        name : "Dr. Anjali Gopakumar",
+        designation : "Associate Professor",
+        description : "Dr. Anjali Gopakumar is an Associate Professor in the School of Science and Humanities at VIT Chennai. She has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Anjali.jpg",
+        email : ""
+      },
+      {
+        name : "K. Vasugi",
+        designation : "Associate Professor",
+        description : "K. Vasugi is an Associate Professor in the School of Science and Humanities at VIT Chennai. She has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Vasugi.jpg",
+        email : ""
+      }
+    ]
+  },
+  {
+    name : "LiLACS",
+    description : "LiLACS is the school of Social Sciences and Languages at VIT Chennai. It is one of the most active schools in the campus. The school has a very active student body and has been organizing various events and workshops for the stuents",
+    image : "/logos/lilacs.webp",
+    faculty : [
+      {
+        name : "Dr. Mahalakshmi Tiwari",
+        designation : "Associate Professor",
+        description : "Dr. Mahalakshmi Tiwari is an Associate Professor in the School of Social Sciences and Languages at VIT Chennai. She has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Mahalakshmi.jpg",
+        email : ""
+      },
+      {
+        name : "Dr. Maya Rathnasabapathy",
+        designation : "Associate Professor",
+        description : "Dr. Maya Rathnasabapathy is an Associate Professor in the School of Social Sciences and Languages at VIT Chennai. She has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Maya.jpg",
+        email : ""
+      }
+    ]
+  },
+  {
+    name : "Taikuun",
+    description : "Taikuun is the school of Business at VIT Chennai. It is one of the most active schools in the campus. The school has a very active student body and has been organizing various events and workshops for the stuents",
+    image : "/logos/taikuun.webp",
+    faculty : [
+      {
+        name : "Dr. Padmavathy C",
+        designation : "Associate Professor",
+        description : "Dr. Padmavathy C is an Associate Professor in the School of Business at VIT Chennai. She has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Padmavathy.jpg",
+        email : ""
+      },
+      {
+        name : "Dr. Buvanesh C",
+        designation : "Associate Professor",
+        description : "Dr. Buvanesh C is an Associate Professor in the School of Business at VIT Chennai. He has been a part of the school since its inception and has been a great support to the students.",
+        link : "/images/faculty/schools/Buvanesh.jpg",
+        email : ""
+      }
+    ]
+  }
+]
+
--- src/components/Schools.tsx
+++ src/components/Schools.tsx
@@ -1,51 +1,116 @@
-"use client";
-import bandvit from "@/assets/logos/bandvit.webp";
-import connect from "@/assets/logos/connectivitieee.webp";
-import diseno from "@/assets/logos/diseno.webp";
-import glitz from "@/assets/logos/glitz.webp";
-import lilacs from "@/assets/logos/lilacs.webp";
-import qubit from "@/assets/logos/qubit.webp";
-import srishti from "@/assets/logos/srishti.webp";
-import taikunn from "@/assets/logos/taikuun.webp";
-import vitness from "@/assets/logos/vitness.webp";
-import vsplash from "@/assets/logos/vsplash.webp";
-import Image, { StaticImageData } from "next/image";
-import { ReactNode } from "react";
+"use client"
+import { Fragment, useRef, useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import Image from 'next/image';
+import { ImageProps, schools } from "../../public/data/data";
 
-interface ImageProps {
-  image: StaticImageData;
-  className?: string;
+function Modal(props: ImageProps & { open: boolean, setOpen: (open: boolean) => void }) {
+  const cancelButtonRef = useRef(null);
+
+  return (
+    <>
+      <Transition.Root show={props.open} as={Fragment}>
+        <Dialog onClose={() => props.setOpen(false)} initialFocus={cancelButtonRef}>
+          <div className="fixed z-10 inset-0 overflow-y-auto">
+            <div className="flex items-center justify-center min-h-screen">
+              <Transition.Child
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+              </Transition.Child>
+              <Transition.Child
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                  <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                    <div className="sm:flex sm:items-start">
+                      <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                          Coordinators
+                        </Dialog.Title>
+                        <div className="mt-2">
+                          {props.faculty.map((faculty) => (
+                            <div key={faculty.name}>
+                              <Image
+                                className="sm:max-w-[8em] sm:max-h-[8em] md:max-h-[8em] md:max-w-[8em] grayscale hover:grayscale-0 hover:scale-125 transition-all duration-300 cursor-pointer"
+                                src={faculty.link}
+                                alt="Logo"
+                                width={200}
+                                height={200}
+                              />
+                              <p className="text-sm text-gray-500">
+                                {faculty.name}
+                              </p>
+                              <p className="text-sm text-gray-500">
+                                {faculty.designation}
+                              </p>
+                              <p className="text-sm text-gray-500">
+                                {faculty.description}
+                              </p>
+                              <p className="text-sm text-gray-500">
+                                {faculty.email}
+                              </p>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    </>
+  );
 }
 
-let ImageComponent = (props: ImageProps): ReactNode => {
+
+const ImageComponent = (props: ImageProps): React.ReactNode => {
+  const [open, setOpen] = useState(false);
+
+  const handleImageClick = () => {
+    if (!open) {
+      setOpen(true);
+    }
+  };
+
   return (
-    <Image
-      className={`sm:max-w-[8em] sm:max-h-[8em] md:max-h-[8em] md:max-w-[8em] grayscale-0 hover:grayscale-0 hover:scale-125 transition-all duration-300 cursor-pointer ${props.className}`}
-      src={props.image}
-      alt="Logo"
-    />
+    <>
+      <Image
+        onClick={handleImageClick}
+        className={`sm:max-w-[8em] sm:max-h-[8em] md:max-h-[8em] md:max-w-[8em] grayscale hover:grayscale-0 hover:scale-125 transition-all duration-300 cursor-pointer ${props.className}`}
+        src={props.image}
+        alt="Logo"
+        width={200}
+        height={200}
+      />
+      <Modal {...props} open={open} setOpen={setOpen} />
+    </>
   );
 };
 
-let Schools = (): JSX.Element => {
+const Schools = (): JSX.Element => {
   return (
     <section className="py-32 sm:py-40" id="#events">
       <section className="md:text-center sm:text-center mb-10 lg:px-32 sm:px-8 md:px-16 text-6xl font-monty bg-clip-text text-transparent bg-gradient-to-t from-stone-600 to-white uppercase ">
         School Fests
       </section>
       <section className="grid md:grid-cols-4 md:px-6 sm:grid-cols-1 lg:grid-cols-4 lg:px-32 items-center place-items-center place-content-center gap-4">
-        <ImageComponent image={qubit} />
-        <ImageComponent image={srishti} />
-        <ImageComponent image={glitz} />
-        <ImageComponent image={taikunn} />
-        <ImageComponent image={diseno} />
-        <ImageComponent image={bandvit} />
-        <ImageComponent image={vsplash} />
-        <ImageComponent image={vitness} />
-      </section>
-      <section className="flex sm:flex-col sm:h-full sm:justify-center sm:items-center px-8 justify-center gap-16">
-        <ImageComponent className="lg:h-52 lg:w-52" image={lilacs} />
-        <ImageComponent className="lg:h-52 lg:w-52" image={connect} />
+        {schools.map((school) => (
+          <ImageComponent key={school.image} {...school} />
+        ))}
       </section>
     </section>
   );
