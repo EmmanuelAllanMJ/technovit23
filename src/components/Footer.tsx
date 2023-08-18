902be94878ce9faf3f48ff31d5473157a5c71b8c
404salad <404salad@gmail.com>
2023-08-18 15:45:51 +0530

    made some nav links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = (): JSX.Element => {
   return (
     <>
-      <div className="flex md:flex-col sm:flex-col flex-row justify-between px-10 md:px-8 sm:px-4 font-monty md:pt-60 sm:pt-96 pt-20 pb-10">
+      <div id = "contact" className="flex md:flex-col sm:flex-col flex-row justify-between px-10 md:px-8 sm:px-4 font-monty md:pt-60 sm:pt-96 pt-20 pb-10">
         <div className="flex w-full flex-col gap-4 md:py-8 sm:py-8">
           <div className="w-full flex items-start justify-center flex-col font-regular text-6xl md:text-5xl sm:text-4xl uppercase bg-clip-text text-transparent bg-gradient-to-tr hover:bg-gradient-to-bl from-stone-600 to-white">
             Contact Us
@@ -20,11 +20,10 @@ const Footer = (): JSX.Element => {
           </div>
         </div>
         <div className="w-full flex flex-col justify-center items-end md:items-start sm:items-start uppercase font-monty text-white gap-12 sm:gap-4 md:gap-8 text-2xl md:text-xl sm:text-sm md:py-8 sm:py-8 sm:border-white sm:border-t-white">
-          <div>Home</div>
-          <div>About</div>
-          <div>Events</div>
-          <div>Gallery</div>
-          <div>Contact Us</div>
+          <div><a href="/">Home</a></div>
+          <div><a href="/about" >About </a> </div>
+          <div><a href="events">Events</a> </div>
+          <div><a href="/#contact">Contact Us</a> </div>
         </div>
         <div />
       </div>
@@ -36,3 +35,21 @@ const Footer = (): JSX.Element => {
 };
 
 export default Footer;
+const navItems = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Events",
+    href: "events",
+  },
+  {
+    title: "Contact",
+    href: "/#contact",
+  },
+];
