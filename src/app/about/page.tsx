557448bb5a64f87984510648dd719f41b7e9f41a
Kushagra Garg <kushagra.garg77@gmail.com>
2023-08-22 22:57:10 +0530

    Merch Added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import FacultyComponent from "@/components/Faculty";
 import React from "react";
-import {Card, CardFooter, Image, Button} from "@nextui-org/react";
+// import {Card, CardFooter, Image, Button} from "@nextui-org/react";
 
 import DelegatesMessage from "@/components/DelegatesMessage";
 
