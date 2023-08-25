366ad6e333407fb875e5cd3190280e218f289719
Abhijith Ganesh <git.watch.abhijith@outlook.com>
2023-08-25 23:58:07 +0530

    Addition of events
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,20 +1,13 @@
 "use client";
 import type { Event } from "@/hooks/getEvents";
+import useEvents from "@/hooks/getEvents";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import CustomDropdown from "./Dropdown";
 import EventCard from "./EventCard";
-import useEvents from "@/hooks/getEvents";
 
 interface EventsProps {
   seemore?: boolean;
-}
-interface IJSONReponse {
-  data: {
-    eventContentCollection: {
-      items: Event[];
-    };
-  };
 }
 
 const Events: React.FC<EventsProps> = ({ seemore }) => {
@@ -74,6 +67,7 @@ const Events: React.FC<EventsProps> = ({ seemore }) => {
   };
 
   const handleSchoolSelect = (value: string) => {
+    console.log(value)
     setSelectedSchool(value);
   };
 
@@ -84,70 +78,64 @@ const Events: React.FC<EventsProps> = ({ seemore }) => {
   if (loading) {
     return <>Loading ...</>;
   } else {
-    console.log(filteredEvents);
     return (
-      <>
-        <section className="relative min-h-screen" id="events">
-          <img
-            src="/assets/events-bg.png"
-            className="h-full w-screen object-cover blur-1xl absolute top-0 left-0 z-0"
-            alt="Background"
-          />
+      <section className="relative min-h-screen" id="events">
+        <img
+          src="/assets/events-bg.png"
+          className="h-full w-screen object-cover blur-1xl absolute top-0 left-0 z-0"
+          alt="Background"
+        />
 
-          <div className="flex justify-center items-center relative z-10">
-            <section className="pt-4 md:text-center sm:text-center mb-10 lg:px-32 sm:px-8 md:px-16 text-6xl font-monty bg-clip-text text-transparent bg-gradient-to-t from-stone-600 to-white">
-              Events
-            </section>
-          </div>
-          <section className="font-monty relative z-10 w-full flex flex-col items-center justify-center">
-            <section className="flex justify-center sm:flex-col md:flex-row w-full lg:flex-row items-center text-white py-7 gap-2 mb-8 lg:w-3/4 md:w-3/4 sm:w-5/6 mx-auto ">
-              {/* Search Bar */}
-              <section className="flex w-full h-16 md:w-1/2 lg-w-1/2">
-                <input
-                  className="bg-white bg-opacity-40 rounded-full py-3 px-3 w-full"
-                  type="search"
-                  placeholder="Search Event Name..."
-                  value={searchQuery}
-                  onChange={handleSearchChange}
-                />
-              </section>
-
-              {/* School Dropdown */}
-              <section className="flex flex-col md:flex-row lg:flex-row z-10 w-full items-start justify-center gap-2 md:mr-3 md:w-1/2 lg-w-1/2 ">
-                <CustomDropdown
-                  label="Schools"
-                  options={[
-                    { value: "", label: "All Schools" },
-                    { value: "Qubit", label: "Qubit [Scope]" },
-                    { value: "Sense", label: "Diseno [Sense]" },
-                  ]}
-                  selectedValue={selectedSchool}
-                  onSelect={handleSchoolSelect}
-                />
-
-                {/* Price Dropdown */}
-                <CustomDropdown
-                  label="Price"
-                  options={[
-                    { value: "", label: "Price" },
-                    { value: "Low", label: "Low" },
-                    { value: "Medium", label: "Medium" },
-                    { value: "High", label: "High" },
-                  ]}
-                  selectedValue={selectedPrice}
-                  onSelect={handlePriceSelect}
-                />
-              </section>
+        <div className="flex justify-center items-center relative z-10">
+          <section className="pt-4 md:text-center sm:text-center mb-10 lg:px-32 sm:px-8 md:px-16 text-6xl font-monty bg-clip-text text-transparent bg-gradient-to-t from-stone-600 to-white">
+            Events
+          </section>
+        </div>
+        <section className="font-monty relative z-10 w-full flex flex-col items-center justify-center">
+          <section className="flex justify-center sm:flex-col md:flex-row w-full lg:flex-row items-center text-white py-7 gap-2 mb-8 lg:w-3/4 md:w-3/4 sm:w-5/6 mx-auto ">
+            <section className="flex w-full h-16 md:w-1/2 lg-w-1/2">
+              <input
+                className="bg-white bg-opacity-40 rounded-full py-3 px-3 w-full"
+                type="search"
+                placeholder="Search Event Name..."
+                value={searchQuery}
+                onChange={handleSearchChange}
+              />
             </section>
 
-            <section className="flex flex-wrap justify-center items-center gap-7">
-              {filteredEvents!?.length === 0 ? (
-                <p className="text-white text-center py-10">
-                  NO EVENTS AVAILABLE
-                </p>
-              ) : seem === 0 ? (
-                filteredEvents!?.map((event, index) =>
-                  index <= 3 ? (
+            {/* School Dropdown */}
+            <section className="flex flex-col md:flex-row lg:flex-row z-10 w-full items-start justify-center gap-2 md:mr-3 md:w-1/2 lg-w-1/2 ">
+              <CustomDropdown
+                label="Schools"
+                options={[
+                  { value: "", label: "All Schools" },
+                  { value: "Qubit", label: "Qubit [Scope]" },
+                  { value: "Sense", label: "Diseno [Sense]" },
+                ]}
+                selectedValue={selectedSchool}
+                onSelect={handleSchoolSelect}
+              />
+
+              {/* Price Dropdown */}
+              <CustomDropdown
+                label="Price"
+                options={[
+                  { value: "", label: "Price" },
+                  { value: "Low", label: "Low" },
+                  { value: "Medium", label: "Medium" },
+                  { value: "High", label: "High" },
+                ]}
+                selectedValue={selectedPrice}
+                onSelect={handlePriceSelect}
+              />
+            </section>
+          </section>
+
+          <section className="flex flex-wrap justify-center items-center gap-7">
+            {filteredEvents!?.length === 0 ? (
+              <>
+                {event_hook.map((event: Event, index: number) => {
+                  return (
                     <EventCard
                       key={index}
                       index={index}
@@ -157,12 +145,12 @@ const Events: React.FC<EventsProps> = ({ seemore }) => {
                       eventSchool={event.school}
                       eventPrice={event.price}
                     />
-                  ) : (
-                    ""
-                  )
-                )
-              ) : (
-                filteredEvents!?.map((event, index) => (
+                  );
+                })}
+              </>
+            ) : seem === 0 ? (
+              filteredEvents!?.map((event, index) =>
+                index <= 3 ? (
                   <EventCard
                     key={index}
                     index={index}
@@ -172,24 +160,38 @@ const Events: React.FC<EventsProps> = ({ seemore }) => {
                     eventSchool={event.school}
                     eventPrice={event.price}
                   />
-                ))
-              )}
-            </section>
+                ) : (
+                  ""
+                )
+              )
+            ) : (
+              filteredEvents!?.map((event, index) => (
+                <EventCard
+                  key={index}
+                  index={index}
+                  eventName={event.eventName}
+                  eventImage={event.imageLink}
+                  eventDescription={event.description}
+                  eventSchool={event.school}
+                  eventPrice={event.price}
+                />
+              ))
+            )}
+          </section>
 
-            <section className="flex justify-center items-center text-center mb-4">
-              <button
-                className="mt-6 mb-4 ml-2 rounded-[90px] border-2 border-purple-600 w-52
+          <section className="flex justify-center items-center text-center mb-4">
+            <button
+              className="mt-6 mb-4 ml-2 rounded-[90px] border-2 border-purple-600 w-52
             h-16 sm:w-44 sm:h-14 md:w-48 md:h-16 lg:w-52 lg:h-16 flex justify-center 
             items-center text-[#C8B8EC] text-base font-medium cursor-pointer 
             bg-opacity-80 hover:bg-purple-500/10 hover:text-purple-200 transition-all duration-300"
-                onClick={seemore ? more : () => router.push("/events")}
-              >
-                {mesg}
-              </button>
-            </section>
+              onClick={seemore ? more : () => router.push("/events")}
+            >
+              {mesg}
+            </button>
           </section>
         </section>
-      </>
+      </section>
     );
   }
 };
--- src/components/Schools.tsx
+++ src/components/Schools.tsx
@@ -1,103 +1,109 @@
-"use client"
-import { Fragment, useRef, useState } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
-import Image from 'next/image';
+"use client";
+import { Dialog, Transition } from "@headlessui/react";
+import Image from "next/image";
+import { Fragment, useRef, useState } from "react";
 import { ImageProps, schools } from "../../public/data/data";
 
-function Modal(props: ImageProps & { open: boolean, setOpen: (open: boolean) => void }) {
+function Modal(
+  props: ImageProps & { open: boolean; setOpen: (open: boolean) => void }
+) {
   const cancelButtonRef = useRef(null);
 
   return (
-    <>
-      <Transition.Root show={props.open} as={Fragment}>
-        <Dialog onClose={() => props.setOpen(false)} initialFocus={cancelButtonRef}>
-          <div className="fixed z-10 inset-0  overflow-y-auto ">
-            <div className="flex flex-wrap items-center justify-center min-h-screen mx-16 my-16">
-              <Transition.Child
-                enter="ease-out duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <Dialog.Overlay className="fixed inset-0 " />
-              </Transition.Child>
-              <Transition.Child
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                enterTo="opacity-100 translate-y-0 sm:scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              >
-                <div className="flex items-center justify-center relative  z-50  p-6 my-8 text-left align-middle transition-all transform rounded-lg shadow-xl sm:my-0 sm:align-top sm:p-8 violet-modal">
-                  <div className="px-2 py-5 sm:p-6  ">
-                    <div className="flex flex-wrap justify-center items-center">
-                      <div className="mt-2 text-center ">
-                        <Dialog.Title as="h3" className="text-5xl sm:text-xl md:text-3xl font-monty bg-clip-text text-transparent bg-gradient-to-t from-stone-600 to-white uppercase">
-                          {props.name} Faculty Coordinators
-                        </Dialog.Title>
-                        <div className={`flex flex-wrap justify-center items-center mt-8 gap-8 `}>
-                            {props.faculty.map((faculty) => (
-                              <div key={faculty.name} className="mb-4 text-center">
-                                <div className="flex items-center justify-center">
-                                  <Image
-                                    className="w-24 h-24 rounded-full"
-                                    src={faculty.link}
-                                    alt="Faculty Image"
-                                    width={96}
-                                    height={96}
-                                  />
-                                </div>
-                                <p className="text-sm font-bold text-white mt-4">
-                                  {faculty.name}
-                                </p>
-                                <p className="text-sm font-semibold text-gray-200">
-                                  {faculty.designation}
-                                </p>
-                                <p className="text-sm text-gray-300 mb-2">
-                                  {faculty.email}
-                                </p>
-                                {/* <div className="text-sm text-justify text-[#aaa6c3] w-48 mx-auto">
-                                  {faculty.description}
-                                </div> */}
-                              </div>
-                            ))}
-                          </div>
-                      </div>
-                    </div>
-                  </div>
-                  <button
-                    onClick={() => props.setOpen(false)}
-                    className="absolute top-4 right-4 text-gray-400 hover:text-gray-600 focus:outline-none"
+    <Transition.Root show={props.open} as={Fragment}>
+      <Dialog
+        onClose={() => props.setOpen(false)}
+        initialFocus={cancelButtonRef}
+      >
+        <section className="fixed z-10 inset-0  overflow-y-auto ">
+          <section className="flex flex-wrap items-center justify-center min-h-screen mx-16 my-16">
+            <Transition.Child
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 " />
+            </Transition.Child>
+            <Transition.Child
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <section className="flex items-center justify-center relative z-50 p-6 my-8 text-left align-middle transition-all transform rounded-lg shadow-xl sm:my-0 sm:align-top sm:p-8 violet-modal">
+                <section className="px-2 py-5 sm:p-6  ">
+                  <section className="flex flex-wrap justify-center items-center">
+                    <section className="mt-2 text-center ">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-5xl sm:text-xl md:text-3xl font-monty bg-clip-text text-transparent bg-gradient-to-t from-stone-600 to-white uppercase"
+                      >
+                        {props.name} Faculty Coordinators
+                      </Dialog.Title>
+                      <section
+                        className={`flex flex-wrap justify-center items-center mt-8 gap-8 `}
+                      >
+                        {props.faculty.map((faculty) => (
+                          <section
+                            key={faculty.name}
+                            className="mb-4 text-center"
+                          >
+                            <section className="flex items-center justify-center">
+                              <Image
+                                className="h-32 w-32 rounded-full"
+                                src={faculty.link}
+                                alt="Faculty Image"
+                                width={108}
+                                height={108}
+                              />
+                            </section>
+                            <p className="text-xl font-extrabold text-white mt-4">
+                              {faculty.name}
+                            </p>
+                            <p className="text-md font-semibold text-gray-200">
+                              {faculty.designation}
+                            </p>
+                            <p className="text-sm text-gray-300 mb-2">
+                              {faculty.email}
+                            </p>
+                          </section>
+                        ))}
+                      </section>
+                    </section>
+                  </section>
+                </section>
+                <button
+                  onClick={() => props.setOpen(false)}
+                  className="absolute top-4 right-4 text-gray-400 hover:text-gray-600 focus:outline-none"
+                >
+                  <svg
+                    className="w-6 h-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
                   >
-                    <svg
-                      className="w-6 h-6"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  </button>
-                </div>
-              </Transition.Child>
-            </div>
-          </div>
-        </Dialog>
-      </Transition.Root>
-    </>
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </section>
+            </Transition.Child>
+          </section>
+        </section>
+      </Dialog>
+    </Transition.Root>
   );
 }
-
-
 
 const ImageComponent = (props: ImageProps): React.ReactNode => {
   const [open, setOpen] = useState(false);
@@ -130,7 +136,7 @@ const Schools = (): JSX.Element => {
         School Fests
       </section>
       <section className="grid md:grid-cols-4 md:px-6 sm:grid-cols-1 lg:grid-cols-4 lg:px-32 items-center place-items-center place-content-center gap-4">
-      <div className=" absolute shadow-purple-700 opacity-30 md:opacity:10 sm:opacity:5 w-1/3 h-400 shadow-[0_0_20em_20em] "></div>
+        <section className=" absolute shadow-purple-700 opacity-30 md:opacity:10 sm:opacity:5 w-1/3 h-400 shadow-[0_0_20em_20em]" />
         {schools.map((school) => (
           <ImageComponent key={school.image} {...school} />
         ))}
