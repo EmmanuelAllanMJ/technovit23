8ff83671fab6d8b8877dd6079ca1890d490f0e40
EmmanuelAllanMJ <emmanuelallanmj@gmail.com>
2023-08-26 00:57:08 +0530

    Added contentful events to the website

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,10 +1,45 @@
 import Events from "@/components/Events";
 import React from "react";
 
-const EventsPage = () => {
+export const dynamic = 'force-dynamic'
+export const fetchCache = 'force-no-store'
+
+const EventsPage = async () => {
+
+  const query = `
+    query {
+      eventContentCollection {
+        items {
+          eventName
+          school
+          description
+          venue
+          price
+          datetime
+          imageLink
+          link
+        }
+      }
+    }
+  `;
+  const res = await fetch(
+    "https://graphql.contentful.com/content/v1/spaces/fh8dptt5f1p3/",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer PfIiyQLVPV1qip8E8fz_fjEQKt1VClDImjAg2eCwo64`,
+      },
+      body: JSON.stringify({ query }),
+    },
+  );
+  const data = await res.json();
+  const events = data.data.eventContentCollection.items;
+  console.log(events);
+  
   return (
     <div className="bg-black bg-[url('/assets/events-bg.png')] overflow-hidden">
-      <Events seemore={true} />
+      <Events seemore={true} events={events} />
     </div>
   );
 };
